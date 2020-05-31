787bedd7f5675a6a1f4cba5eee9b6f00a033a9c7
Alvin <marangachweya@gmail.com>
2020-05-31 09:10:35 +0300

    New Text & Image

--- link.php
+++ link.php
@@ -3,8 +3,19 @@
 <link rel="stylesheet" href="link.css">
 </head>
 <body>
-<p>
-<img src="User.png" alt="Profile" height="50" width="50" class="image"></p>
-&nbsp;
+<img src="User.png" alt="Profile" height="50" width="50" style="float:right;" class="userimage">
+<p class="webname" style="width: 217px"><strong>Tinkernut Web Pages &copy; 2020</strong></p>
 </body>
+<style>
+.userimage{
+    border-radius: 50%;
+}
+.webname{
+    border-style: double;
+}
+body{
+background-color: #393635;
+color:white;
+}
+</style>
 </html>
